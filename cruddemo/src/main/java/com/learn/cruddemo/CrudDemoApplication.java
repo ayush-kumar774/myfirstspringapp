95c5172f11c0ac2c92d437bbb4cf9ee80ec64999
Aayush Kumar <53551688+ayush-kumar774@users.noreply.github.com>
2023-05-01 10:12:02 +0530

    Added function to create multiple student object and save them in database

--- cruddemo/src/main/java/com/learn/cruddemo/CrudDemoApplication.java
+++ cruddemo/src/main/java/com/learn/cruddemo/CrudDemoApplication.java
@@ -17,13 +17,14 @@ public class CrudDemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+			//createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
 		// create the student object
-		System.out.println("Creating new student object ...");
+		System.out.println("Creating multiple student objects ...");
 		Student tempStudent = new Student("Paul", "Deo", "paul@luv2code.com");
 
 		// save the student object
@@ -32,6 +33,26 @@ public class CrudDemoApplication {
 
 		// display id of the saved student
 		System.out.println("Saved student. Generated id: " + tempStudent.getId());
+	}
+
+	private void createMultipleStudents (StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent1 = new Student("Steve", "Rogers", "captain.america@avengers.com");
+		Student tempStudent2 = new Student("Tony", "Stark", "ironman@avengers.com");
+		Student tempStudent3 = new Student("Natasha", "Roman-off", "black.widow@avengers.com");
+
+		// save the student object
+		System.out.println("Saving the students ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+
+		// display id of the saved student
+		System.out.println("Saved students.");
+		System.out.println("Generated id #1 = : " + tempStudent1.getId());
+		System.out.println("Generated id #2 = : " + tempStudent2.getId());
+		System.out.println("Generated id #3 = : " + tempStudent3.getId());
 	}
 
 }
