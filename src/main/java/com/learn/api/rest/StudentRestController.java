9860d33b7e009d9a4b42725a4beee4adba96f807
Aayush Kumar <53551688+ayush-kumar774@users.noreply.github.com>
2023-05-27 11:42:08 +0530

    modified the controller

--- src/main/java/com/learn/api/rest/StudentRestController.java
+++ src/main/java/com/learn/api/rest/StudentRestController.java
@@ -44,13 +44,27 @@ public class StudentRestController {
 
     @ExceptionHandler
     public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exception) {
+        // create a StudentErrorResponse
         StudentErrorResponse errorResponse = new StudentErrorResponse();
 
         errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
         errorResponse.setMessage(exception.getMessage());
         errorResponse.setTimeStamp(System.currentTimeMillis());
 
+        // return ResponseEntity
         return new ResponseEntity<StudentErrorResponse>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException (Exception exception) {
+        // create a StudentErrorResponse
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setMessage(exception.getMessage());
+        errorResponse.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<StudentErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
